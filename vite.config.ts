553e8bb45133f4e4d68c95c6f6f5534f124491d6
Zealotic-spec <askiloff10@gmail.com>
2026-06-14 15:18:44 +0500

    add electron build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: './',
     // Prevent Vite from clearing the terminal so Rust and Tauri diagnostic lines remain visible
     clearScreen: false,
 
